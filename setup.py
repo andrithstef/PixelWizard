7b10908a2fd2f737713acc4ca752eb41b729c17c
andriths <andri@hefringmarine.com>
2023-06-21 21:13:43 +0000

    v 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='pixelwizard',
-        version='0.1.0',
+        version='1.0.0',
         author='andrithstef',
         description='A simple image editor',
         packages=find_packages(include=['pixelwizard', 'pixelwizard.*']),
